99c2fa720a1afb78732277cd52751aca0261d3f2
dhduncan <dhduncan1975@gmail.com>
2021-09-28 21:10:20 +1000

    Tweaks to align ttiq prediction and case-based estimates

--- R/gg_tp_reduction.R
+++ R/gg_tp_reduction.R
@@ -11,12 +11,28 @@ gg_tp_reduction <- function(scenario_df_run_tp_multiplier) {
 
   cases_tp_reduction <- scenario_df_run_tp_multiplier %>% 
     relocate(time_to_isolation_sims) %>% 
-    mutate(time_to_isolation_sims = map(time_to_isolation_sims, c)) %>% 
+    mutate(time_to_isolation_sims = map(time_to_isolation_sims, c)) %>%
     unnest(cols = time_to_isolation_sims) %>% 
-    mutate(scenario = as_factor(scenario),
-           scenario = fct_relevel(scenario,
-                                  "optimal",
-                                  "current"))
+    mutate(scenario = factor(scenario, 
+                             levels = c("optimal",
+                                        "current_case_init",
+                                        "current"),
+                             labels = c("Optimal",
+                                        "NSW Current with case-initiated",
+                                        "NSW Current without case-initiated")),
+          ) %>%
+    mutate(time_to_isolation_sims = floor(time_to_isolation_sims)) %>%
+    group_by(scenario, time_to_isolation_sims) %>%
+    summarise(
+      tp_multiplier = first(tp_multiplier),
+      count = n(),
+        ) %>%
+    drop_na %>%
+    mutate(
+      fraction = count/sum(count)
+    ) %>%
+    ungroup()
+    
   
   df_annotate <- cases_tp_reduction %>% 
     group_by(scenario) %>% 
@@ -33,31 +49,36 @@ gg_tp_reduction <- function(scenario_df_run_tp_multiplier) {
     distinct() %>% 
     arrange(scenario) %>% 
     mutate(x = c(13, 1, 13),
-           y = c(5000, 3000, 4000))
+           y = c(.15, .15, .15))
+  
+  #match_cols <- manual_pal(values = c("#1B9E77", "#7570B3", "#7570B3"))
   
   ggplot(cases_tp_reduction,
-         aes(x = time_to_isolation_sims,
-             fill = scenario)) +
-    geom_histogram(colour = "white",
-                   binwidth = 1) + 
+         aes(x = time_to_isolation_sims, y = fraction, fill = scenario)) +
+    geom_vline(
+      xintercept = 5,
+      linetype = 3,
+      col = grey(0.5)
+      ) +
+    geom_col() + 
     facet_wrap(~ scenario,
-               ncol = 1,
-               scales = "free_y") + 
-    theme_bw() + 
+               ncol = 3,
+               labeller = label_wrap_gen(width=15)
+               ) + 
+    scale_y_continuous(labels = scales::percent_format(accuracy = 1)) +
+    theme_minimal() + 
     labs(
       fill = "",
-      y = "Count",
-      # x = "# days from isolation to infection"
-      x = "# days from infection to isolation"
+      y = "Cases isolated",
+      x = "Days since infection"
     ) + 
-    scale_fill_brewer(
-      palette = "Dark2"
-    ) + 
+    scale_fill_brewer(palette = "Dark2") + 
     lims(
       x = c(-1,14)
-    ) + 
+    ) +
     geom_text(data = df_annotate,
               aes(x = x, y = y, label = message)) +
-    theme(legend.position = "none")
+    theme(legend.position = "none") + 
+    ggtitle("Times to isolation from model")
 
 }
