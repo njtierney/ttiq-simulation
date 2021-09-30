d03d3c39131d3b0865c9197a69014987dfc4699f
Logan Wu <logan.wu@dhhs.vic.gov.au>
2021-09-30 11:51:46 +1000

    Move plot colouring into tribble

--- R/gg_tp_reduction.R
+++ R/gg_tp_reduction.R
@@ -11,28 +11,27 @@ gg_tp_reduction <- function(scenario_df_run_tp_multiplier) {
   
   dark2 = brewer.pal(8, "Dark2")
   # Define plotting constants in order
-  scenario_names = c("Optimal (NSW)", "Partial (VIC)",
-                     "NSW Current\nwith case-initiated",
-                     "NSW Current\nwithout case-initiated",
-                     "VIC Current\nwith case-initiated", 
-                     "VIC Current\nwithout case-initiated")
-  scenario_values = c("optimal", "partial", "current_nsw_case_init", "current_nsw",
-                      "current_vic_case_init", "current_vic") %>%
-    setNames(scenario_names)
-  scenario_colors = c(dark2[1], dark2[2], dark2[3], dark2[3], dark2[4], dark2[4])
+  scenarios = tribble(
+    ~name, ~value, ~colour,
+    "Optimal (NSW)", "optimal", dark2[1],
+    "Partial (VIC)", "partial", dark2[2],
+    "NSW Current\nwith case-initiated", "current_nsw_case_init", dark2[3],
+    "NSW Current\nwithout case-initiated", "current_nsw", dark2[3],
+    "VIC Current\nwith case-initiated", "current_vic_case_init", dark2[4],
+    "VIC Current\nwithout case-initiated", "current_vic", dark2[4]
+  ) %>%
+    mutate(name = fct_inorder(name))
   
   cases_tp_reduction <- scenario_df_run_tp_multiplier %>% 
     relocate(time_to_isolation_sims) %>% 
     mutate(time_to_isolation_sims = map(time_to_isolation_sims, c)) %>%
     unnest(cols = time_to_isolation_sims) %>% 
+    left_join(scenarios, by=c("scenario"="value")) %>%
     mutate(
-      scenario = fct_recode(scenario, !!!scenario_values) %>%
-        fct_relevel(scenario_names)
-    ) %>%
-    mutate(
+      scenario = name,
       time_to_isolation_sims = floor(time_to_isolation_sims)
     ) %>%
-    group_by(scenario, time_to_isolation_sims) %>%
+    group_by(scenario, colour, time_to_isolation_sims) %>%
     summarise(
       count = n(),
       tp_multiplier = first(tp_multiplier),
@@ -42,40 +41,32 @@ gg_tp_reduction <- function(scenario_df_run_tp_multiplier) {
     mutate(
       fraction = count / sum(count)
     ) %>%
-    ungroup() %>%
-    mutate(
-      scenario_colour = case_when(
-        scenario == "Optimal" ~ 1,
-        TRUE ~ 2
-      ),
-      scenario_colour = factor(scenario_colour)
-    ) %>%
     ungroup()
   
   
   df_annotate <- cases_tp_reduction %>% 
-    group_by(scenario) %>% 
+    group_by(scenario, colour) %>% 
     summarise(
       avg_days = weighted.mean(time_to_isolation_sims, fraction),
       tp_multiplier = first(tp_multiplier),
+      .groups = "drop"
     ) %>% 
     ungroup() %>% 
     mutate(tp_reduction = glue("{percent(1 - tp_multiplier, accuracy = 1)} reduction"),
            avg_days = glue("{round(avg_days)} day average"),
            message = glue("{tp_reduction}\n{avg_days}")) %>% 
-    relocate(tp_reduction) %>% 
-    select(tp_reduction,
-           scenario,
-           avg_days,
-           message) %>% 
-    distinct() %>%
-    arrange(scenario)
+    relocate(tp_reduction) %>%
+    distinct(tp_reduction,
+             scenario,
+             avg_days,
+             message,
+             colour)
   
   ggplot(cases_tp_reduction,
          aes(
            x = time_to_isolation_sims,
            y = fraction,
-           fill = scenario
+           fill = colour
          )) +
     geom_vline(
       xintercept = 5,
@@ -100,12 +91,13 @@ gg_tp_reduction <- function(scenario_df_run_tp_multiplier) {
       y = "Cases isolated",
       x = "Days since infection"
     ) + 
-    scale_fill_manual(values=scenario_colors) +
+    scale_fill_identity() +
     lims(
       x = c(-1,14)
     ) +
     geom_label(data = df_annotate,
-               aes(x = Inf, y = Inf, label=message), hjust="inward", vjust="inward", color="white", label.size=NA) +
+               aes(x = Inf, y = Inf, label=message),
+               hjust="inward", vjust="inward", color="white", label.size=NA) +
     theme(legend.position = "none") + 
     ggtitle("Times to isolation from model")
 }
