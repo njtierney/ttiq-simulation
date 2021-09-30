6d6fed2d90db51c6e0f6a592cbff5a8a1ef990f8
Logan Wu <logan.wu@dhhs.vic.gov.au>
2021-09-30 10:37:45 +1000

    Split function out into file

--- R/dist_params_to_df.R
+++ R/dist_params_to_df.R
@@ -1,27 +1,3 @@
-#' Generic method to turn different cases of distributions into character strings
-#' 
-#' `if` cases are currently set to the different distributions seen in the data.
-#' 
-#' @param x Distribution object
-#' 
-#' @return A customised character string
-#' @export
-as.character.distribution <- function(x) {
-  sapply(x, function(xx) {
-    if (inherits(xx, "dist_poisson")) {
-      glue::glue("l={xx$l}")
-    } else {
-      if (inherits(xx$dist[[2]]$dist, "dist_poisson"))
-        pois_dist = xx$dist[[2]]$dist
-      else {
-        pois_dist = xx$dist[[2]]$dist[[2]]$dist
-      }
-      glue::glue("l={pois_dist$l},p={xx$w[2][[1]]}")
-    }
-  }) %>%
-    as.character()
-}
-
 #' Convert a dataframe of distributions into a dataframe of parameters
 #' 
 #' @param derived_delay_distributions Dataframe with a scenario column and other
