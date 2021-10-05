c149ce057259f6321fe7c224ee757c8c850ce6f5
Logan Wu <logan.wu@dhhs.vic.gov.au>
2021-10-05 12:05:34 +1100

    Convert to DT

--- R/sim_tracing.R
+++ R/sim_tracing.R
@@ -45,42 +45,49 @@ sim_tracing <- function(cases_scenario, derived_delay_distributions, n_samples=1
     .swab_date = numeric(0)
     i = 1
     while (length(.swab_date) < n_samples) {
-      .swab_date = c(.swab_date, rep(i, rpois(1, rate)))
+      # .swab_date = c(.swab_date, rep(i, rpois(1, rate)))
+      .swab_date = c(.swab_date, rep(i, max(0, rnorm(1, rate, sqrt(0.25*rate)))))
       i = i + 1
     }
     # Notify Dept after test is processed
     scenario_samples = scenario_samples %>%
       mutate(swab_date = .swab_date[seq_len(n_samples)],
              notification_date = swab_date + samples_test_turnaround_time)
+    setDT(scenario_samples)
     
     # Simulate queue
     message(glue("Simulating queue for {max(scenario_samples$notification_date)} iterations/days"))
+    pb = txtProgressBar(min = 1, max = max(scenario_samples$notification_date), initial = 1) 
     for (sim_day in seq_len(max(scenario_samples$notification_date))) {
+      setTxtProgressBar(pb, sim_day)
       
-      scenario_samples = scenario_samples %>%
-        # Prioritise cases (in order of `arrange` arguments)
-        arrange(
-          # Put cases that have been notified and with no interview first. This
-          # ensures that all interviewable cases are above non-interviewable cases
-          desc(notification_date <= sim_day & is.na(interview_date) & notification_date > (sim_day-14)),
-          # Prioritise cases from the past week
-          # desc(swab_date >= (sim_day - 7)),
-          # Prioritise cases in the priority proportion. Accounts for a delay in receiving information about
-          # priority, e.g. because of followup calls or manual classification
-          desc(priority_group & (notification_date + priority_info_delay) <= sim_day),
-          # Prioritise today's notifications
-          desc(notification_date >= sim_day),
-          # Finally prioritise most recent swabs (Jennie Musto, Health NSW)
-          desc(swab_date),
-        ) %>%
-        # Interview the maximum number of cases that can be interviewed
-        mutate(interview_date = ifelse(
-          # extra `if` criteria ensure non-interviewable cases are not interviewed
-          is.na(interview_date) & notification_date <= sim_day & row_number() <= capacity,
-          # probability to push interview to next day 
-          ifelse(runif(n()) < 0.3, sim_day + 1, sim_day),
-          interview_date))
+      # scenario_samples = scenario_samples %>%
+      #   arrange(
+      #     desc(notification_date <= sim_day & is.na(interview_date) & notification_date > (sim_day-14)),
+      #     desc(priority_group & (notification_date + priority_info_delay) <= sim_day),
+      #     desc(notification_date >= sim_day),
+      #     desc(swab_date),
+      #   ) %>%
+      #   mutate(interview_date = ifelse(
+      #     is.na(interview_date) & notification_date <= sim_day & row_number() <= capacity,
+      #     ifelse(runif(n()) < 0.3, sim_day + 1, sim_day),
+      #     interview_date))
+      
+      # DT version
+      # Arrange in order of priorities
+      scenario_samples <- scenario_samples[order(scenario_samples[, .(notification_date <= sim_day & is.na(interview_date) & notification_date > (sim_day-14), 
+                                                                      priority_group & (notification_date + priority_info_delay) <= sim_day,
+                                                                      notification_date >= sim_day,
+                                                                      swab_date)],
+                                                 decreasing = T),]
+      # Set the first `capacity` to be interviewed if not already interviewed
+      scenario_samples[,row_number := 1:.N][, interview_date := as.numeric(interview_date)][ 
+        is.na(interview_date) & notification_date <= sim_day & row_number <= capacity, 
+        # Fraction get interviewed on the next day if they are too late in the day
+        # Currently affects all interviews. Maybe it should only affect day 0 interviews?
+        interview_date := ifelse(runif(.N) < 0.3, sim_day + 1, sim_day)]
     }
+    close(pb)
     
     # Assemble output dataframe
     tibble(scenario = scenario,
@@ -97,28 +104,28 @@ sim_tracing <- function(cases_scenario, derived_delay_distributions, n_samples=1
   
   
   # Diagnostic plots
-  {
-    scenario_samples = generated_samples %>%
-      filter(scenario == "current_nsw") %>%
-      unnest(cols = starts_with("samples_"))
-    xmax = max(scenario_samples$samples_time_to_interview)
-    p1 = ggplot(scenario_samples, aes(x=samples_time_to_interview, fill=samples_time_to_interview >= 0)) +
-      geom_bar() +
-      coord_cartesian(xlim=c(-2, NA)) +
-      scale_x_continuous(breaks = c(-2, 0:xmax), labels=c("Missed", 0:xmax)) +
-      labs(title = glue("Interview capacity of {capacity/rate} x rate"),
-           fill = "Interviewed\n(to show NAs)")
-
-    p2 = ggplot(scenario_samples, aes(x=samples_time_to_interview, fill=samples_time_to_interview >= 0)) +
-      geom_bar() +
-      facet_wrap(vars(samples_priority_group)) +
-      coord_cartesian(xlim=c(-2, NA)) +
-      scale_x_continuous(breaks = c(-2, 0:xmax), labels=c("Missed", 0:xmax)) +
-      labs(title = glue("Facet by priority group"),
-           fill = "Interviewed\n(to show NAs)")
-
-    p1 / p2
-  }
+  # {
+  #   scenario_samples = generated_samples %>%
+  #     filter(scenario == "current_nsw") %>%
+  #     unnest(cols = starts_with("samples_"))
+  #   xmax = max(scenario_samples$samples_time_to_interview)
+  #   p1 = ggplot(scenario_samples, aes(x=samples_time_to_interview, fill=samples_time_to_interview >= 0)) +
+  #     geom_bar() +
+  #     coord_cartesian(xlim=c(-2, NA)) +
+  #     scale_x_continuous(breaks = c(-2, 0:xmax), labels=c("Missed", 0:xmax)) +
+  #     labs(title = glue("Interview capacity of {capacity/rate} x rate"),
+  #          fill = "Interviewed\n(to show NAs)")
+  #   
+  #   p2 = ggplot(scenario_samples, aes(x=samples_time_to_interview, fill=samples_time_to_interview >= 0)) +
+  #     geom_bar() +
+  #     facet_wrap(vars(samples_priority_group)) +
+  #     coord_cartesian(xlim=c(-2, NA)) +
+  #     scale_x_continuous(breaks = c(-2, 0:xmax), labels=c("Missed", 0:xmax)) +
+  #     labs(title = glue("Facet by priority group"),
+  #          fill = "Interviewed\n(to show NAs)")
+  #   
+  #   p1 / p2
+  #   }
 }
 
 # # From Nick Golding on Slack
