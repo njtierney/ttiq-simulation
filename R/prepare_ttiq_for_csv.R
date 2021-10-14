08b1e9a6277fe08b3148c79c61f15f635d154c02
Nicholas Tierney <nicholas.tierney@gmail.com>
2021-10-14 14:43:18 +0800

    TTIQ saved CSV is changed to just look at the time to isolation sims, which is what I think we want.

--- R/prepare_ttiq_for_csv.R
+++ R/prepare_ttiq_for_csv.R
@@ -12,11 +12,13 @@ prepare_ttiq_for_csv <- function(scenario_df_run_tp_multiplier) {
   scenario_df_run_tp_multiplier %>% 
     select(-sim_tracing_fun, 
            -passive_distribution) %>% 
-    relocate(scenario) %>% 
+    relocate(scenario,
+             time_to_isolation_sims) %>% 
+    select(-samples,
+           -time_to_active,
+           -time_to_passive) %>% 
     unnest(
-      cols = c(time_to_isolation_sims,
-               time_to_active,
-               time_to_passive)
+      cols = c(time_to_isolation_sims)
     ) %>% 
     relocate(p_missed, 
              .after = r_start)
