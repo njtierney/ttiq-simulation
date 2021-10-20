0931a546ae583884ff46f9906fac985560003dc6
Nick Golding <goldingn35@gmail.com>
2021-10-20 19:03:14 +0800

    add in symptomatic effects

--- R/infectiousness.R
+++ R/infectiousness.R
@@ -12,8 +12,29 @@ infectiousness <- function(infections, day) {
   # compute infectiousness from GI distribution, isolation status, vaccination_status etc.
   
   day_diff <- day - infections$infection_day
-  isolation_multiplier <- ifelse(isolated(infections, day), 0, 1)
-  vaccination_multiplier <- ifelse(infections$vaccinated, .abm_parameters$ve_onward, 1)
-  .abm_parameters$R * gi_pmf_discrete(day_diff) * isolation_multiplier * vaccination_multiplier
+  
+  isolation_multiplier <- ifelse(
+    isolated(infections, day),
+    0,
+    1
+  )
+  
+  vaccination_multiplier <- ifelse(
+    infections$vaccinated,
+    .abm_parameters$ve_onward,
+    1
+  )
+  
+  symptomatic_multiplier <- ifelse(
+    infections$symptomatic,
+    .abm_parameters$asymptomatic_relative_infectiousness,
+    1
+  )
+  
+  .abm_parameters$R_star *
+    gi_pmf_discrete(day_diff) *
+    isolation_multiplier *
+    vaccination_multiplier *
+    symptomatic_multiplier
   
 }
--- R/setup_abm.R
+++ R/setup_abm.R
@@ -14,14 +14,18 @@ setup_abm <- function(...) {
   
   # defining default arguments
   args <- list(
-    R = 1.5,
-    ve_onward = 0.4,
-    ve_susceptibility = 0.8,
-    vaccination_coverage = 0.5,
+    R = 3.62,
+    # incorporate correction for onward transmission to account for reduction in
+    # infectiousness due to being symptomatic
+    ve_onward = 0.5 * 0.6,
+    ve_susceptibility = 0.73,
+    vaccination_coverage = 0.8,
+    asymptomatic_relative_infectiousness = 0.5,
     clinical_fraction = 0.4,
     n_days = 365
   )
   
+  # check validity of arguments
   set_args <- names(new_args)
   possible_args <- names(args)
   invalid <- !set_args %in% possible_args
@@ -32,13 +36,19 @@ setup_abm <- function(...) {
     )
   }
   
-  
+  # update defaults
   updated <- possible_args %in% set_args
-  
   if (length(updated) > 0) {
     args <- args[!updated]
     args <- c(args, new_args)
   }
+  
+  # correct R for reduced infectiousness of symptomatics
+  args$R_star <- with(
+    args,
+    R / ((1 - clinical_fraction) + clinical_fraction * asymptomatic_relative_infectiousness)
+  )
+  
   args
   
 }
