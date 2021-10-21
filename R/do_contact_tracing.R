dc91aa9195ea472924e94c2585cafabacb63f278
Nick Golding <goldingn35@gmail.com>
2021-10-21 10:55:57 +0800

    fix sampling of contact tracing delays

--- R/do_contact_tracing.R
+++ R/do_contact_tracing.R
@@ -12,47 +12,46 @@ do_contact_tracing <- function(infections) {
   # for all infections put into isolation today, assign their infectees an
   # isolation date
   
-  # which infections were isolated today?
-  source_isolated_today <- infections$isolation_day == .abm_globals$day
-  source_ids <- infections$id[source_isolated_today]
+  # sample an interview day for all infections
+  source_time_to_interview <- times_to_interview(infections)
   
-  # sample an interview day for each of these
-  source_time_to_interview <- times_to_interview(infections[source_isolated_today, ])
-  
-  # find the unisolated infectees of these infections
-  
-  # sample whether they would be identified in the interview
+  # sample a probability of detection for all infections
   contact_would_be_identified <- rbinom(
     nrow(infections),
     1,
     .abm_parameters$p_active_detection
   )
   
-  # determine who is isolated
-  contact_isolated <- infections$source_id %in% source_ids &
+  # sample a different time to isolation for all infections
+  contact_interview_to_isolation <- rpois(nrow(infections), 0.5)
+  
+  
+  # which infections were isolated today?
+  source_isolated_today <- infections$isolation_day == .abm_globals$day
+  traced_source_ids <- infections$id[source_isolated_today]
+  
+  # index from each infection to these sources (many will be NA)
+  traced_source_idx <- match(infections$source_id, traced_source_ids)
+  
+  # get the day of isolation for the contacts that are found, using the delay
+  # from their source
+  contact_isolation_day <- .abm_globals$day +
+    source_time_to_interview[traced_source_idx] +
+    contact_interview_to_isolation
+  
+  # determine who will be identified by contact tracing today
+  contact_isolated <- infections$source_id %in% traced_source_ids &
     !is.finite(infections$isolation_day) &
     contact_would_be_identified
   
-  n_contacts_isolated <- sum(contact_isolated)
-  # look up their date of isolation
-  
-  # index from the contacts to the sources
-  source_idx <- match(infections$source_id[contact_isolated], source_ids)
-  
-  # sample a different time to isolation for each (infected) contact
-  contact_interview_to_isolation <- rpois(n_contacts_isolated, 0.5)
-  
-  # get the day of isolation for the contacts that are found
-  contact_isolation_day <- .abm_globals$day +
-    source_time_to_interview[source_idx] +
-    contact_interview_to_isolation
-
+  # only use the isolation days for those isolated today
   infections$isolation_day <- ifelse(
     contact_isolated,
     contact_isolation_day,
     infections$isolation_day
   )
   
+  # set their reason for detection
   infections$case_found_by <- ifelse(
     contact_isolated,
     "contact_tracing",
